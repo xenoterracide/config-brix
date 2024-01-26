32d33236c78207f42d3604ead339a4bd0c988521
Caleb Cushing <xenoterracide@gmail.com>
2024-01-26 16:46:51 -0500

    chore: update files

--- java/project/shared/settings.gradle.kts
+++ java/project/shared/settings.gradle.kts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright © 2023-2024 Caleb Cushing.
-rootProject.name = "ai-wh40k"
+rootProject.name = "PROJECT"
 
 rootDir.resolve("module").listFiles()?.forEach { file ->
   if (file.isDirectory && file?.list { _, name -> name == "build.gradle.kts" }
