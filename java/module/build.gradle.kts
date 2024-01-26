bd1336f9f66c41810e1a8786dcabf9b20008c9a0
Caleb Cushing <xenoterracide@gmail.com>
2024-01-26 16:13:47 -0500

    fix: seems to be working

--- java/module/build.gradle.kts
+++ java/module/build.gradle.kts
@@ -1,19 +1,40 @@
-group = "TLD"
-version = "0.1.0-SNAPSHOT"
+// SPDX-License-Identifier: MIT
+// Copyright © 2023-2024 Caleb Cushing.
+buildscript { dependencyLocking.lockAllConfigurations() }
 
-plugins {
-  id("our.java-library")
-}
+plugins { our.javalibrary }
 
 dependencies {
+  runtimeOnly(platform(libs.spring.platform))
+  runtimeOnly(libs.starter.core)
+  runtimeOnly(libs.starter.log4j2)
+  runtimeOnly(libs.starter.webflux)
+  runtimeOnly(libs.starter.actuator)
+
+  api(platform(libs.spring.platform))
+  api(libs.spring.boot.autoconfigure)
+
   implementation(platform(libs.spring.platform))
   implementation(libs.spring.boot.autoconfigure)
-  runtimeOnly(libs.spring.boot.starter.core)
-  runtimeOnly(libs.spring.boot.starter.log4j2)
-  testImplementation(libs.compile.testing)
+  implementation(libs.spring.boot.core)
+  implementation(libs.spring.context)
+
+  testImplementation(platform(libs.spring.platform))
+  testImplementation(libs.bundles.spring.test)
+  testImplementation(libs.junit.api)
+  testImplementation(libs.spring.beans)
+
   modules {
     module("org.springframework.boot:spring-boot-starter-logging") {
-      replacedBy("org.springframework.boot:spring-boot-starter-log4j2", "Use Log4j2 instead of Logback")
+      replacedBy(
+        "org.springframework.boot:spring-boot-starter-log4j2",
+        "Use Log4j2 instead of Logback",
+      )
     }
   }
+}
+
+tasks.withType<JacocoCoverageVerification>().configureEach {
+  dependsOn(project.tasks.withType<JacocoReport>())
+  violationRules { rule { limit { minimum = 0.3.toBigDecimal() } } }
 }
