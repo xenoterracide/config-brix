519711ddbb2fc30c4fe21a782a2040a781a76f56
Caleb Cushing <xenoterracide@gmail.com>
2024-02-23 22:14:10 -0500

    chore: update exludes

--- sync-from.sh
+++ sync-from.sh
@@ -3,6 +3,9 @@ SOURCE=$1
 rsync --archive --update --delete --verbose --human-readable \
   --exclude "sync-from.sh" \
   --exclude "sync-to.sh" \
-  --exclude ".git" \
+  --exclude ".git/" \
   --exclude "*lockfile" \
+  --exclude "node_modules/" \
+  --exclude "build/" \
+  --exclude ".gradle/" \
   "$SOURCE/" .
